f3f61c557be7eac4dd1cbb57fa62ce15dfe4c399
Kraisorn Apisaranont <kraisorn@vedanetwork.com>
2017-03-16 18:48:56 +0700

    Test local commit

--- typings/interfaces.d.ts
+++ typings/interfaces.d.ts
@@ -1,77 +1,55 @@
-/**
- * Created by kraisorna on 3/16/2017 AD.
- */
-//Test local commit.
-//Interfaces for getBalance function of InfobipSMS class.
 interface AccountBalance {
     balance: number;
     currency: string;
 }
-
-//Interfaces for getPreview function of InfobipSMS class.
 interface Language {
     languageCode: string;
 }
-
 interface PreviewConfiguration {
     language: Array<Language>;
     transliteration: string;
 }
-
 interface Preview {
     textPreview: string;
     messageCount: number;
     charactersRemaining: number;
     configuration: Array<PreviewConfiguration>;
 }
-
-interface MessagePreview{
+interface MessagePreview {
     originalText: string;
     previews: Array<Preview>;
 }
-
-//Interfaces for sendOne function of InfobipSMS class.
-interface MessageStatus{
+interface MessageStatus {
     groupId: number;
     groupName: string;
     id: number;
     name: string;
     description: string;
 }
-
 interface Message {
     to: string;
     status: MessageStatus;
     smsCount: number;
     messageId: string;
 }
-
 interface SendOneResult {
     messages: Array<Message>;
 }
-
-//Interfaces for sendMulti function of InfobipSMS class.
 interface SendMultiResult {
     bulkId: string;
     messages: Array<Message>;
 }
-
-//Interfaces for sendAdvanced function of InfobipSMS class.
 interface SendAdvancedResult {
     bulkId: string;
     messages: Array<Message>;
 }
-
-//Interfaces for getReports function of InfobipSMS class.
-interface MessagePrice{
+interface MessagePrice {
     pricePerMessage: number;
     currency: string;
 }
-
-interface MessageError extends MessageStatus{
+interface MessageError extends MessageStatus {
     permanent: boolean;
 }
-
 interface DeliveryResult {
     messageId: string;
     to: string;
@@ -84,13 +62,10 @@ interface DeliveryResult {
     status: MessageStatus;
     error: MessageError;
 }
-
-//Interfaces for getNumberContext function of InfobipSMS class.
-interface MobileNetwork{
+interface MobileNetwork {
     networkPrefix: number;
     countryPrefix: number;
 }
-
 interface NumberResult {
     to: string;
     mccMnc: string;
